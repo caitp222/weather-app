5c3fb338c8f2e88e5af1ba3a2e737e8c472c986b
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-17 20:18:25 -0400

    Remove hard-coded new york location

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,8 +6,8 @@ end
 
 get '/' do
   @places = Place.all
-  # @current_location = Place.find_by(name: find_location)
-  @current_location = Place.find_by(name: "New York")
+  @current_location = Place.find_by(name: find_location)
+  # @current_location = Place.find_by(name: "New York")
   puts @current_location.name
   puts @current_location.open_weather_code
   erb :'index'
